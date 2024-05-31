102c812b2485f6dbd62ff32cc9703d0082169053
Alexey Lukashevich <las900372@gmail.com>
2024-05-31 16:26:54 +0300

    Report
    Продолжаю работу над сценой допроса

--- report.gd
+++ report.gd
@@ -18,6 +18,7 @@ var player_answers := {
 	killer_nickname = ""
 }
 
+
 func _notification(what):
 	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
 		hide()
@@ -29,39 +30,29 @@ func _input(_event):
 		hide()
 
 
+# Игрок ввёл текст в поле ответа нв вопрос 1 - показать кнопку подтверждения
 func _on_Answer1_text_changed(new_text):
 	if not new_text.empty():
 		get_node("%Button1").show()
+	else:
+		get_node("%Button1").hide()
 
 
 func _on_Button1_pressed():
 	player_answers["chat_name"] = get_node("%Answer1").text
+	
+	# Если при нажатии "подтвердить" поле с ответом не пустое - показать вопрос 2
 	if not player_answers["chat_name"].empty():
 		get_node("%Question2").show()
 		get_node("%Answer2").show()
 		get_node("%MarginContainer2").show()
-
-
-func _on_Answer2_text_changed(new_text):
-	if not new_text.empty():
-		get_node("%Button2").show()
-
-
-func _on_Button2_pressed():
-	player_answers["total_players"] = int(get_node("%Answer2").text)
-	print(player_answers["total_players"])
-	if not player_answers["total_players"] == 0:
-		get_node("%Question3").show()
-		get_node("%Answer3").show()
-		get_node("%MarginContainer3").show()
-
-
-func _on_Answer3_text_entered(new_text):
-	player_answers["players_nicknames"] = new_text.split(',', false)
 	
+	# Если игрок ввёл правильный ответ - скрыть кнопку подтверждения
+	if player_answers["chat_name"].to_lower() == correct_answers["chat_name"]:
+		get_node("%Answer1").editable = false
+		get_node("%Button1").hide()
 
 
-
-
-
-
+func _on_BackButton_pressed():
+	hide()
+	emit_signal("back_pressed")
