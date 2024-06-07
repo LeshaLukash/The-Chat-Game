ff6b82918fa26a3e9bdcbfac68c9e055bb7e3844
Alexey Lukashevich <las900372@gmail.com>
2024-06-07 16:38:41 +0300

    Report, Intro, перевод
    Исправил недочёты в тексте интро, начал заново работу над сценой допроса: игроку не нужно будет дотошно указывать данные обо ВСЕХ игрока, а лишь ответить на парочку каверзных вопросов. Внёс поправки в translation.csv

--- report.gd
+++ report.gd
@@ -4,7 +4,7 @@ signal back_pressed
 
 var correct_answers := {
 	chat_name = "игра на выбывание",
-	total_players = 5,
+	total_players = 6,
 	players_nicknames = [],
 	victim_nickname = "saxxxy13",
 	killer_nickname = ""
@@ -28,29 +28,6 @@ func _notification(what):
 func _input(_event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		hide()
-
-
-# Игрок ввёл текст в поле ответа нв вопрос 1 - показать кнопку подтверждения
-func _on_Answer1_text_changed(new_text):
-	if not new_text.empty():
-		get_node("%Button1").show()
-	else:
-		get_node("%Button1").hide()
-
-
-func _on_Button1_pressed():
-	player_answers["chat_name"] = get_node("%Answer1").text
-	
-	# Если при нажатии "подтвердить" поле с ответом не пустое - показать вопрос 2
-	if not player_answers["chat_name"].empty():
-		get_node("%Question2").show()
-		get_node("%Answer2").show()
-		get_node("%MarginContainer2").show()
-	
-	# Если игрок ввёл правильный ответ - скрыть кнопку подтверждения
-	if player_answers["chat_name"].to_lower() == correct_answers["chat_name"]:
-		get_node("%Answer1").editable = false
-		get_node("%Button1").hide()
 
 
 func _on_BackButton_pressed():
